75acef88fbb570e81a32dcaf6cf7cd88ef1ea807
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2015-08-09 17:02:58 -0400

    removed the secret!

--- oauth/main/GitHubOAuth/configs.js
+++ oauth/main/GitHubOAuth/configs.js
@@ -1,6 +1,6 @@
 module.exports = {
 	clientId: '0dc6c0334e18e5ff3802',
-	clientSecret: '57caefed11e9964700287d5e246364a41e84fd23',
+	clientSecret: '',
 	scope: 'public_repo,user',
 	AUTHORIZE: 'https://github.com/login/oauth/authorize',
 	ACCESS_TOKEN_URL: 'https://github.com/login/oauth/access_token'
